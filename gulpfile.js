6274989ff16c27810f3fbaef9a7aa6d75072e163
Iván Barcia <ivan@barcia.cc>
2018-03-16 19:04:43 +0100

    Rewrite gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,97 +1,151 @@
 // Dependencies
-var gulp         = require('gulp');
-var rename       = require('gulp-rename');
-var notify       = require("gulp-notify");
-var plumber      = require('gulp-plumber');
-var sass         = require('gulp-sass');
-var sourcemaps   = require('gulp-sourcemaps');
-let uglify       = require('gulp-uglify-es').default;
-var postcss      = require('gulp-postcss');
-var cssnano      = require('cssnano');
-var browserSync  = require('browser-sync').create();
-
-// Paths
-var sourcePath = './src/';
-var distPath   = './dist/';
-
-// Tasks
-var cssTask         = 'css';
-var jsTask          = 'js';
-var allTask         = 'all';
-var browserSyncTask = 'sync';
+const gulp         = require('gulp');
+const rename       = require('gulp-rename');
+const notify       = require("gulp-notify");
+const plumber      = require('gulp-plumber');
+const sass         = require('gulp-sass');
+const sourcemaps   = require('gulp-sourcemaps');
+const uglify       = require('gulp-uglify-es').default;
+const postcss      = require('gulp-postcss');
+const cssnano      = require('cssnano');
+const browserSync  = require('browser-sync').create();
 
 
-
-var onError = function (err) {
-  notify({
-    title: 'Gulp: Sass error'
-  }).write(err);
-  console.log(err.toString());
-  this.emit('end');
+// Path
+const path = {
+  src: './src/',
+  dist: './dist/',
+  docs: './docs/'
 };
 
 
-// Compile Scss files to CSS, and apply PostCSS plugins
-gulp.task(cssTask, function () {
-  var postcssPlugins = [
-    cssnano({
-      autoprefixer: false
-    })
-  ];
-  var sassOptions = {
-    outputStyle: 'expanded'
-  };
+// Sourcefiles
+const sourcefiles = {
+  css: path.src + 'swibe.scss',
+  js: path.src + 'swibe.js'
+};
 
-  return gulp.src(sourcePath+'/swibe.scss')
-  .pipe(plumber({ errorHandle: onError }))
-  .pipe(sourcemaps.init())
-  .pipe(sass(sassOptions).on('error', onError))
-  .pipe(gulp.dest(distPath))
-  .pipe(postcss(postcssPlugins))
-  .pipe(rename({
-    suffix: '.min'
-  }))
-  .pipe(sourcemaps.write('./'))
-  .pipe(gulp.dest(distPath));
-});
+
+// Tasks
+const task = {
+  css: 'css',
+  js: 'js',
+  all: 'build'
+};
 
 
 
-// Concatenate and minify JS files
-gulp.task(jsTask, function() {
-  return gulp.src(sourcePath+'swibe.js')
-  .pipe(sourcemaps.init())
-  .pipe(uglify())
-  .pipe(sourcemaps.write('./'))
-  .pipe(rename({
-    suffix: '.min'
-  }))
-  .pipe(gulp.dest(distPath));
-});
+
+
+// FUNCTIONS
+
+/**
+ * Build all
+ */
+const build = function() {
+  cssTask();
+	jsTask();
+	console.log("Builded All");
+}
 
 
 
-// Watch
+/**
+ * Error function
+ */
+const reportError = function (error) {
+	notify({
+		title: 'Gulp error',
+		subtitle: error.plugin,
+		message: error.message,
+		sound: 'Basso',
+		timeout: 3
+	}).write(error);
+	console.error(error.toString());
+	this.emit('end');
+};
+
+
+
+/**
+ * Init browserSync
+ */
+const initBrowserSync = function() {
+		browserSync.init({
+			browser: 'google chrome',
+			server: './'
+		});
+};
+
+
+
+/**
+ * Watch
+ */
+const watch = function() {
+	gulp.watch(path.docs + '*.html', [browserSync.reload]);
+	gulp.watch(sourcefiles.css, [task.css]);
+	gulp.watch(sourcefiles.js, [task.js]);
+};
+
+
+
+/**
+ * CSS
+ */
+const cssTask = function() {
+	return gulp.src(sourcefiles.css)
+	.pipe(plumber({errorHandler: reportError}))
+	.pipe(sourcemaps.init())
+	.pipe(sass())
+  .pipe(gulp.dest(path.dist))
+	.pipe(postcss([
+		cssnano({
+			autoprefixer: false,
+			safe: true,
+			sourcemap: false
+		})
+	]))
+	.pipe(rename({
+		suffix: '.min'
+	}))
+	.pipe(sourcemaps.write('./'))
+	.pipe(gulp.dest(path.dist))
+	.pipe(browserSync.stream());
+};
+
+
+
+/**
+ * JS
+ */
+const jsTask = function() {
+	return gulp.src(sourcefiles.js)
+	.pipe(plumber({errorHandler: reportError}))
+	.pipe(sourcemaps.init())
+	.pipe(uglify({}))
+	.pipe(rename({
+		suffix: '.min'
+	}))
+	.pipe(sourcemaps.write('./'))
+	.pipe(gulp.dest(path.dist))
+	.pipe(browserSync.stream());
+};
+
+
+
+
+
+/**
+ * TASKS
+ */
+
+// Default - Build all, start BrowserSync and Watch
 gulp.task('default', function () {
-  gulp.watch(sourcePath+'swibe.scss', [cssTask]);
-  gulp.watch(sourcePath+'swibe.js', [jsTask]);
+	initBrowserSync();
+	watch();
 });
 
-
-
-// Browser Sync
-gulp.task(browserSyncTask, function() {
-    browserSync.init({
-        server: {
-            baseDir: './'
-        }
-    });
-
-    gulp.watch(distPath+'style.min.css').on('change', browserSync.reload);
-    gulp.watch(distPath+'script.min.js').on('change', browserSync.reload);
-    gulp.watch('*.html').on('change', browserSync.reload);
-});
-
-
-
-gulp.task(allTask, [cssTask, jsTask]);
+gulp.task(task.all, function() { build() });
+gulp.task(task.css, function() { cssTask() });
+gulp.task(task.js, function() { jsTask() });
